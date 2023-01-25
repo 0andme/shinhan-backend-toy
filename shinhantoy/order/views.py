b14a464f5b38b5d9f2d051befc597d98b7d952f4
0andme <yu00mi97@gmail.com>
2023-01-25 15:49:13 +0900

    #2 feat : order list GET apt 추가

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework import generics,mixins
+from .serializer import OrderSerializer
+from .models import Order
 # Create your views here.
+
+
+class OrderListView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView,
+):
+
+    serializer_class=OrderSerializer
+    def get_queryset(self):
+
+        orders=Order.objects.all().order_by('id')
+
+        return  orders
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,args,kwargs)
+  
