5195164129c1a09f89a35873c5fba82fc512fe62
0andme <yu00mi97@gmail.com>
2023-01-26 16:52:37 +0900

    #6 feat : 주문 상세 페이지 관련 API 추가
    - 주문 내역 전체 데이터 조회GET
    - 주문 내역 별 댓글 전체 목록 조회 GET
    - 주문 내역 별 댓글 추가 POST

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics,mixins
-from .serializer import OrderSerializer
-from .models import Order
-# Create your views here.
+from .serializer import OrderSerializer,CommentSerializer,CommentCreateSerializer
+from .models import Order,Comment
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class OrderListView(
@@ -16,4 +17,44 @@ class OrderListView(
 
     def get(self,request,*args,**kwargs):
         return self.list(request,args,kwargs)
-  
+
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView,
+):
+
+    serializer_class=OrderSerializer
+    def get_queryset(self):
+      
+        return Order.objects.all().order_by('-id')
+
+    def get(self,request,*args,**kwargs):
+        print(self.kwargs.get('pk'))
+        return self.retrieve(request,args,kwargs)
+
+
+class CommentListView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class=CommentSerializer
+    def get_queryset(self):
+        order_id=self.kwargs.get('pk')
+        return Comment.objects.filter(order_id=order_id).order_by('-id')
+
+    def get(self,request,*args,**kwargs):
+       return self.list(request,args,kwargs)
+    
+
+class CommentCreateView(
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class=CommentCreateSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_queryset(self):
+        return Comment.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
