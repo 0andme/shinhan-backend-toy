30452a88874c5847f842067de011d4eb14feb6e7
0andme <yu00mi97@gmail.com>
2023-01-26 10:24:40 +0900

    #3 feat : token생성 api 추가

--- shinhantoy/shinhantoy/urls.py
+++ shinhantoy/shinhantoy/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import(
+    TokenVerifyView,
+    TokenRefreshView,
+    TokenObtainPairView,
+)
 
 
 urlpatterns = [
     path('api/order', include('order.urls')),
     path('admin/', admin.site.urls),
+    path('api/token',TokenObtainPairView.as_view()),
 ]
